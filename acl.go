f7f83fcb842f7f191b93c02a21bed415da419ded
Mariusz Gronczewski <xani666@gmail.com>
2016-08-18 13:52:38 +0200

    do not export instance data structures

--- acl.go
+++ acl.go
@@ -1,52 +1,47 @@
 package acl
 
 import (
+	"encoding/json"
 	"fmt"
 	"sync"
-	"encoding/json"
 )
 
 // acl-s are multi-part names that use / as divider
 // for example
 
-
 type Instance struct {
-	Map map[string]*Branch
-	Roles map[string]*Role
-	Permissions map[string]*Permission
+	aclMap      map[string]*Branch
+	roles       map[string]*Role
+	permissions map[string]*Permission
 	sync.RWMutex
 }
 
 func NewInstance() *Instance {
 	var i Instance
-	i.Map = make(map[string]*Branch)
-	i.Roles = make(map[string]*Role)
-	i.Permissions = make(map[string]*Permission)
+	i.aclMap = make(map[string]*Branch)
+	i.roles = make(map[string]*Role)
+	i.permissions = make(map[string]*Permission)
 	return &i
 }
 
-
 const (
-	RoleUser = iota
+	RoleUser  = iota
 	RoleGroup = iota
 	// <- insert new roles here
 	RoleLast = iota //end of enum pointer, used by validator, must be last
 )
 
 type Permission struct {
-	Name string
+	Name       string
 	nameParsed []string
 }
 
 type Acl struct {
-	Name string
+	Name   string
 	Grants []Permission
 }
 
-
-
-
-func (acl *Instance) NewRole(name string, roltype int) ( e error) {
+func (acl *Instance) NewRole(name string, roltype int) (e error) {
 	if roltype >= RoleLast {
 		return fmt.Errorf("Invalid role ID, either you did not use package constants or are trying to import new data into old acl lib!")
 	}
@@ -55,7 +50,7 @@ func (acl *Instance) NewRole(name string, roltype int) ( e error) {
 	r.Type = roltype
 	r.instance = acl // return ref so we can get to role's ACLs of user got role struct
 	acl.Unlock()
-	return  e
+	return e
 }
 
 func (acl *Instance) AddBranch(name string) (err error) {
@@ -67,10 +62,10 @@ func (acl *Instance) AddBranch(name string) (err error) {
 	var currentPtr *Branch
 	for idx, val := range splitP {
 		if idx == 0 {
-			if _, ok := acl.Map[val]; !ok {
-				acl.Map[val]=newBranch(val)
+			if _, ok := acl.aclMap[val]; !ok {
+				acl.aclMap[val] = newBranch(val)
 			}
-			currentPtr = acl.Map[val]
+			currentPtr = acl.aclMap[val]
 			continue
 
 		}
@@ -79,8 +74,6 @@ func (acl *Instance) AddBranch(name string) (err error) {
 			currentPtr.Branch[val] = newBranch(val)
 		}
 		currentPtr = currentPtr.Branch[val]
-
-
 
 	}
 	_ = currentPtr
@@ -91,7 +84,7 @@ func (acl *Instance) NewPermission(name string) (err error) {
 	var perm Permission
 	perm.Name = name
 	acl.Lock()
-	acl.Permissions[name] = &perm
+	acl.permissions[name] = &perm
 	acl.Unlock()
 	return err
 }
@@ -100,11 +93,19 @@ func (acl *Instance) AddPerm(name string, perm string) (err error) {
 	return err
 }
 
-func (acl *Instance) Role(name string) (*Role) {
-	return acl.Roles[name]
+func (acl *Instance) Role(name string) *Role {
+	return acl.roles[name]
 }
 
 func (acl *Instance) DebugDump() string {
-	out,_ := json.MarshalIndent(acl, "", "    ")
+	out, _ := json.MarshalIndent(struct{
+		Map      map[string]*Branch
+		Roles       map[string]*Role
+		Permissions map[string]*Permission
+	}{
+		acl.aclMap,
+		acl.roles,
+		acl.permissions,
+	}, "", "    ")
 	return string(out)
 }
